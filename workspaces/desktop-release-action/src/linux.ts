d48f1621c15ad3e4f93185f82f355dc5b572e9e3
ankar84 <ankar84@gmail.com>
2021-08-11 09:33:34 +0300

    Eslint fix

--- workspaces/desktop-release-action/src/linux.ts
+++ workspaces/desktop-release-action/src/linux.ts
@@ -1,8 +1,2 @@
-import { promises } from 'fs';
-
-import * as core from '@actions/core';
-
-import { run, runElectronBuilder } from './shell';
-
 export const packOnLinux = (): Promise<void> =>
   runElectronBuilder(`--linux tar.gz deb rpm`);
